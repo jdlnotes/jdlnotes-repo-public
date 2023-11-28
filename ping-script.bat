712553e110b56023cc24626037d141dde4683842
Jose Luna Jr <151938371+jdlnotes@users.noreply.github.com>
2023-11-28 15:02:29 +0800

    ping-script

--- ping-script.bat
+++ ping-script.bat
@@ -10,6 +10,15 @@ NOTE:
 Windows 10 and older
 ping 8.8.8.8 -t >>%USERPROFILE%\Desktop\8.8.8.8-ping-result.txt
 
+Windows 10
+ping 8.8.8.8 -t >>%USERPROFILE%\Desktop\8.8.8.8-ping-result.txt
+ping aka.ms -t >>%USERPROFILE%\Desktop\aka.ms-ping-result.txt
+
+============
+
+Windows 11
+ping 8.8.8.8 -t >> C:\Users\(LOGIN ACCOUNT)\Downloads\8.8.8.8-ping-result.txt
+ping aka.ms -t >> C:\Users\(LOGIN ACCOUNT)\Downloads\aka.ms-ping-result.txt
 
 Windows 11
 ping 8.8.8.8 -t >> C:\Users\(LOGIN ACCOUNT)\Downloads\8.8.8.8-ping-result.txt
