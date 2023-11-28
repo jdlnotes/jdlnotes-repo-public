9a3ebf3001031ec673444f034d2fdeceb0adf067
Jose Luna Jr <151938371+jdlnotes@users.noreply.github.com>
2023-11-28 15:19:47 +0800

    ping-script

--- ping-script.bat
+++ ping-script.bat
@@ -22,3 +22,9 @@ ping aka.ms -t >> C:\Users\(LOGIN ACCOUNT)\Downloads\aka.ms-ping-result.txt
 
 Windows 11
 ping 8.8.8.8 -t >> C:\Users\(LOGIN ACCOUNT)\Downloads\8.8.8.8-ping-result.txt
+
+continuous ping USING PSPING
+C:\PSTools>psping -t 8.8.8.8 >> C:\Users\(LOGIN ACCOUNT)\Downloads\8.8.8.8-ping-result.txt
+
+TELNET ON PORT 443 USING PSPING
+C:\PSTools>psping -t 8.8.8.8:443 >> C:\Users\(LOGIN ACCOUNT)\Downloads\8.8.8.8-ping-result.txt
